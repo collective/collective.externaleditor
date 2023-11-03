62c11e19fb866e6e0b966fa673bcf6770e324046
ale-rt <alessandro.pisa@gmail.com>
2023-11-03 09:56:44 +0100

    Fix the build

--- setup.py
+++ setup.py
@@ -46,11 +46,21 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "setuptools",
         "Plone",
+        "Products.CMFCore",
+        "Products.CMFPlone",
         "Products.ExternalEditor>=1.1.0dev",
+        "Products.GenericSetup",
+        "Products.statusmessages",
+        "setuptools",
         # -*- Extra requirements: -*-
     ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.robotframework[debug]",
+        ],
+    },
     python_requires=">=3.7",
     entry_points="""
       # -*- Entry points: -*-
